25746d96941bec37b44384ea70686ff18b4b6682
zPookiePie <agatajorge280206@gmail.com>
2026-09-23 14:26:59 -0300

    style: alterei as fontes dos titulos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next';
+import { Itim } from "next/font/google";
 import './globals.scss';
+
+const itim = Itim({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-itim",
+});
 
 export const metadata: Metadata = {
   title: 'Hora do Descanso',
@@ -7,5 +15,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="pt-BR"><body>{children}</body></html>;
+  return <html lang="pt-BR" className={itim.variable}><body>{children}</body></html>;
 }
